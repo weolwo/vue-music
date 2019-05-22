f8f180b2793be7e3b6ec122eb1c84a9e93f56ff8
poplar <poplar@qq.com>
2019-05-22 08:14:34 +0800

    播放器初始提交

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,6 +3,30 @@ import * as types from './mutation-types'
 const mutations = {
   [types.SET_SINGER] (state, singer) {
     state.singer = singer
-  }
+  },
+
+  [types.SET_FULLSCREEN] (state, flag) {
+    state.fullScreen = flag
+  },
+
+  [types.SET_PLAYLIST] (state, list) {
+    state.playList = list
+  },
+
+  [types.SET_PLAYING] (state, flag) {
+    state.playing = flag
+  },
+
+  [types.SET_SEQUENCELIST] (state, list) {
+    state.sequenceList = list
+  },
+
+  [types.SET_CURRENTINDEX] (state, index) {
+    state.currentIndex = index
+  },
+
+  [types.SET_MODE] (state, mode) {
+    state.mode = mode
+  },
 }
 export default mutations
--- src/store/state.js
+++ src/store/state.js
@@ -1,4 +1,12 @@
+import {playMode} from '../common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  fullScreen: false,
+  playList: [],
+  sequenceList: [],
+  currentIndex: -1,
+  mode: playMode.sequence,
+  playing: false
 }
 export default state
